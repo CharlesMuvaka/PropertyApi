a5825678c2ea0668442923cf49082d7a465dd516
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-30 21:03:45 +0300

    create a my_property database that will containt tenant, maintainance quest, property and property manager relations.

---
+++ create.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE my_property;
+\c
+CREATE TABLE maintenance_quest(
+quest_id serial PRIMARY KEY,
+description VARCHAR,
+quest_url VARCHAR,
+property_name VARCHAR
+);
+
+CREATE TABLE property(
+id serial PRIMARY KEY,
+property_name VARCHAR,
+manager_name VARCHAR
+);
+
+CREATE TABLE property_managers(
+id serial PRIMARY KEY,
+manager_name VARCHAR,
+phone_number int,
+email VARCHAR,
+property_name VARCHAR,
+property_description VARCHAR
+);
+
+CREATE TABLE tenant(
+id serial PRIMARY KEY,
+tenant_name VARCHAR,
+tenant_email VARCHAR,
+property_name VARCHAR
+);
+
