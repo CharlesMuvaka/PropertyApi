4da032cd32d2e7db7ddcfdb05b98941f66ed99c1
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-01 23:51:04 +0300

    create a query to insert a property instance fields in the properties relation.

--- src/main/java/org/example/data/PropertyDao.java
+++ src/main/java/org/example/data/PropertyDao.java
@@ -1,4 +1,24 @@
 package org.example.data;
 
-public class PropertyDao {
+import org.example.dao.PropertyInterface;
+import org.example.models.Property;
+import org.example.models.PropertyManager;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class PropertyDao implements PropertyInterface {
+    @Override
+    public void addProperty(Property property) {
+        String query = "INSERT INTO property(property_name,manager_name) VALUES(:property_name,:manager_name)";
+        try(Connection conn = DB.sql20.open()){
+            property.id = (int) conn.createQuery(query)
+                    .addParameter("property_name", property.property_name)
+                    .addParameter("manager_name", property.manager_name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+
 }
