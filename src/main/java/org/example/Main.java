92bd84aaa3af7f98c3722688f12bae9a19b69a96
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-01 10:04:11 +0300

    create a test to check if a property manager can be successfully added to the managers relation.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.data.PropertyManagerDao;
+import org.example.models.PropertyManager;
+
 public class Main {
     public static void main(String[] args) {
+        PropertyManagerDao propertyManagerDao = new PropertyManagerDao();
+        PropertyManager manager = new PropertyManager("Charles", "0768761610", "muvaka@gmail.com","Nyambu plaza", "Based in katoyoyo");
+//        propertyManagerDao.addPropertyManager(manager);
 
+        System.out.println(propertyManagerDao.getPropertyManagerById(1));
     }
 }
