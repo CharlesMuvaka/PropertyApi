7f8e96329bee4dcfc5a13617607a2ddc712bf4c4
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-01 23:49:36 +0300

    create tests to add and retrieve objects from the database.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,32 @@
 package org.example;
 
+import org.example.data.PropertyDao;
 import org.example.data.PropertyManagerDao;
+import org.example.data.TenantDao;
+import org.example.models.Property;
 import org.example.models.PropertyManager;
+import org.example.models.Tenant;
 
 public class Main {
     public static void main(String[] args) {
+
+
         PropertyManagerDao propertyManagerDao = new PropertyManagerDao();
+        TenantDao tenantDao = new TenantDao();
+        PropertyDao propertyDao = new PropertyDao();
         PropertyManager manager = new PropertyManager("Charles", "0768761610", "muvaka@gmail.com","Nyambu plaza", "Based in katoyoyo");
 //        propertyManagerDao.addPropertyManager(manager);
+        Tenant collo = new Tenant("Collins", "collin@gmail.com", "Nyambu Plaza");
+        Property house = new Property("Nyambu Plaza", "Collins");
+//        tenantDao.addTenant(collo);
+//        propertyDao.addProperty(house);
 
         System.out.println(propertyManagerDao.getPropertyManagerById(1));
+        System.out.println(tenantDao.getAllTenants());
+//        System.out.println(propertyDao.getAllProperties());
+        System.out.println(propertyManagerDao.propertyManagerProperties(house.manager_name));
+
+        
+
     }
 }
