78768a0b155caf1f90a90d55dc02ca38c995cd72
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-01 09:57:20 +0300

    create a connection to the DBMS to add a property manager to the property manager relation.

---
+++ src/main/java/org/example/data/PropertyManagerDao.java
@@ -0,0 +1,30 @@
+package org.example.data;
+
+import org.example.dao.PropertyManagerInterface;
+import org.example.models.Property;
+import org.example.models.PropertyManager;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class PropertyManagerDao implements PropertyManagerInterface {
+
+    @Override
+    public void addPropertyManager(PropertyManager propertymanager) {
+        String query = "INSERT INTO property_managers(manager_name,phone_number,email,property_name,property_description) VALUES(:manager_name,:phone_number,:email,:property_name,:property_description)";
+        try(Connection connection = DB.sql20.open()){
+            int id = (int) connection.createQuery(query,true)
+                    .addParameter("manager_name", propertymanager.manager_name)
+                    .addParameter("phone_number", propertymanager.phone_number)
+                    .addParameter("email", propertymanager.email)
+                    .addParameter("property_name", propertymanager.property_name)
+                    .addParameter("property_description", propertymanager.property_description)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
+
+
+
+}
