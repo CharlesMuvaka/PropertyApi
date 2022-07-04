e1176814765f37a4118f0843eadf51f11ef45500
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-04 08:24:28 +0300

    add a method to help in heroku deployment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,8 +15,19 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Main {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
         PropertyManagerDao propertyManagerDao = new PropertyManagerDao();
         TenantDao tenantDao = new TenantDao();
         PropertyDao propertyDao = new PropertyDao();
