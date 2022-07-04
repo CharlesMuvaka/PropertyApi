bf13a1e080556eba120a858f16cf25eec2fcf872
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-04 09:16:09 +0300

    add heroku postgresql database credentials.

--- src/main/java/org/example/data/DB.java
+++ src/main/java/org/example/data/DB.java
@@ -4,5 +4,6 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-    public static Sql2o sql20 = new Sql2o("jdbc:postgresql://localhost:5432/my_property", "moringa", "Access");
+//    public static Sql2o sql20 = new Sql2o("jdbc:postgresql://localhost:5432/my_property", "moringa", "Access");
+    public static Sql2o sql20 = new Sql2o("jdbc:postgresql://ec2-54-159-22-90.compute-1.amazonaws.com:5432/d6acpspnb6o7k2", "njwfwmgzzqpcfb", "09c1e9e079ab7a33c0c1cc1020341c6eee03c5fedf404af136395c863a364766");
 }
