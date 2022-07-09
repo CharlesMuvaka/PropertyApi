af1517800531fbfe04f8b5183b2595d63aa03512
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-09 07:16:21 +0300

    fix:add property id and unit id to help in retrieving all the tenants in the same property.

--- src/main/java/org/example/data/TenantDao.java
+++ src/main/java/org/example/data/TenantDao.java
@@ -3,20 +3,25 @@ package org.example.data;
 import org.example.dao.TenantInterface;
 import org.example.models.Tenant;
 import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class TenantDao implements TenantInterface {
     @Override
     public void addTenant(Tenant tenant) {
-        String query = "INSERT INTO tenant(tenant_name, tenant_email, property_name) VALUES(:tenant_name,:tenant_email,:property_name)";
+        String query = "INSERT INTO tenant(tenant_name, tenant_email,tenant_phone,tenant_id, property_id,unit_id, joined) VALUES(:tenant_name,:tenant_email,:tenant_phone,:tenant_id,:property_id,:unit_id, now())";
         try(Connection conn = DB.sql20.open()){
-            tenant.id = (int) conn.createQuery(query,true)
-                    .addParameter("tenant_name", tenant.tenant_name)
-                    .addParameter("tenant_email", tenant.tenant_email)
-                    .addParameter("property_name", tenant.property_name)
+            int id = (int) conn.createQuery(query,true)
+                    .addParameter("tenant_name", tenant.getTenant_name())
+                    .addParameter("tenant_email", tenant.getTenant_email())
+                    .addParameter("tenant_phone", tenant.getTenant_phone())
+                    .addParameter("tenant_id", tenant.getTenant_id())
+                    .addParameter("property_id", tenant.getProperty_id())
+                    .addParameter("unit_id", tenant.getUnit_id())
                     .executeUpdate()
                     .getKey();
+            tenant.setId(id);
         }
     }
 
@@ -24,7 +29,7 @@ public class TenantDao implements TenantInterface {
     public Tenant getTenantById(int id) {
         String query = "SELECT * FROM tenant WHERE id = :id";
         try(Connection conn = DB.sql20.open()){
-            return conn.createQuery(query).addParameter("id", id).executeAndFetchFirst(Tenant.class);
+            return conn.createQuery(query).addParameter("id",id).executeAndFetchFirst(Tenant.class);
         }
 
     }
@@ -34,28 +39,48 @@ public class TenantDao implements TenantInterface {
         String query = "SELECT * FROM tenant";
         try(Connection conn = DB.sql20.open()){
             return conn.createQuery(query).executeAndFetch(Tenant.class);
+        }catch (Sql2oException ex){
+            System.out.println(ex.fillInStackTrace().getMessage());
         }
-
+        return null;
     }
 
     @Override
-    public List<Tenant> getTenantsInAProperty(String propertyNAme) {
-        String query  = "SELECT * FROM tenant WHERE property_name = :propertyName";
+    public List<Tenant> getTenantsInAProperty(int id) {
+        String query  = "SELECT * FROM tenant WHERE property_id = :id";
         try(Connection conn = DB.sql20.open()){
-            return conn.createQuery(query).addParameter("propertyName", propertyNAme).executeAndFetch(Tenant.class);
+            return conn.createQuery(query).addParameter("id", id).throwOnMappingFailure(false).executeAndFetch(Tenant.class);
         }
 
     }
 
     @Override
-    public void updateTenant(Tenant tenant) {
+    public void updateTenant(int id,Tenant tenant) {
+        String query = "UPDATE tenant SET tenant_name = :tenant_name, tenant_email = :tenant_email, tenant_phone = :tenant_phone, tenant_id = :tenant_id, property_id = :property_id, unit_id = :unit_id WHERE id = :id";
+        try(Connection conn = DB.sql20.open()){
+            conn.createQuery(query)
+                    .addParameter("id", id)
+                    .addParameter("tenant_name",tenant.getTenant_name() )
+                    .addParameter("tenant_email", tenant.getTenant_email())
+                    .addParameter("tenant_phone", tenant.getTenant_phone())
+                    .addParameter("tenant_id", tenant.getTenant_id())
+                    .addParameter("property_id", tenant.getProperty_id())
+                    .addParameter("unit_id", tenant.getUnit_id())
+                    .executeUpdate();
+        }
 
     }
 
+    @Override
+    public void delete(int id) {
+        String query = "DELETE FROM tenant WHERE id = :id";
 
-
-
-
+        try(Connection conn = DB.sql20.open()){
+            conn.createQuery(query)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
 
 
 }
