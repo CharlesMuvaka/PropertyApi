5b1089e0e638b5cfbb945744b030a087ac9ac9c9
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-09 07:14:00 +0300

    fix:remove unwanted fields from the property managers relation.

--- src/main/java/org/example/data/PropertyManagerDao.java
+++ src/main/java/org/example/data/PropertyManagerDao.java
@@ -11,16 +11,15 @@ public class PropertyManagerDao implements PropertyManagerInterface {
 
     @Override
     public void addPropertyManager(PropertyManager propertymanager) {
-        String query = "INSERT INTO property_managers(manager_name,phone_number,email,property_name,property_description) VALUES(:manager_name,:phone_number,:email,:property_name,:property_description)";
+        String query = "INSERT INTO property_managers(manager_name,phone_number,email) VALUES(:manager_name,:phone_number,:email)";
         try(Connection connection = DB.sql20.open()){
-             propertymanager.id = (int) connection.createQuery(query,true)
-                    .addParameter("manager_name", propertymanager.manager_name)
-                    .addParameter("phone_number", propertymanager.phone_number)
-                    .addParameter("email", propertymanager.email)
-                    .addParameter("property_name", propertymanager.property_name)
-                    .addParameter("property_description", propertymanager.property_description)
+             int id = (int) connection.createQuery(query,true)
+                    .addParameter("manager_name", propertymanager.getManager_name())
+                    .addParameter("phone_number", propertymanager.getPhone_number())
+                    .addParameter("email", propertymanager.getEmail())
                     .executeUpdate()
                     .getKey();
+             propertymanager.setId(id);
         }
 
     }
@@ -29,7 +28,7 @@ public class PropertyManagerDao implements PropertyManagerInterface {
         String query = "SELECT * FROM property_managers WHERE id = :id";
         try(Connection conn = DB.sql20.open()){
 
-            return conn.createQuery(query).addParameter("id", id).executeAndFetchFirst(PropertyManager.class);
+            return conn.createQuery(query).addParameter("id", id).throwOnMappingFailure(false).executeAndFetchFirst(PropertyManager.class);
         }
 
     }
@@ -37,27 +36,41 @@ public class PropertyManagerDao implements PropertyManagerInterface {
     public List<PropertyManager> getAllPropertyManagers() {
         String query = "SELECT * FROM property_managers";
         try(Connection conn = DB.sql20.open()){
-            return conn.createQuery(query).executeAndFetch(PropertyManager.class);
+            return conn.createQuery(query).throwOnMappingFailure(false).executeAndFetch(PropertyManager.class);
         }
 
     }
 
     @Override
-    public List<Property> propertyManagerProperties(String managerName) {
-        String query = "SELECT * FROM property WHERE manager_name = :managerName";
+    public List<Property> propertyManagerProperties(int id) {
+        String query = "SELECT * FROM property WHERE id = :id";
         try(Connection conn = DB.sql20.open()){
-            return conn.createQuery(query).addParameter("managerName", managerName).executeAndFetch(Property.class);
+            return conn.createQuery(query).addParameter("id", id).throwOnMappingFailure(false).executeAndFetch(Property.class);
         }
 
     }
 
     @Override
     public void update(int id, PropertyManager manager) {
-        String query  = "UPDATE property_managers SET manager_name = :manager_name, phone_number = :phone_number, email = :email, ";
+        String query  = "UPDATE property_managers SET manager_name = :manager_name, phone_number = :phone_number, email = :email WHERE id = :id";
+        try(Connection conn = DB.sql20.open()){
+            conn.createQuery(query)
+                    .addParameter("id", id)
+                    .addParameter("manager_name", manager.getManager_name())
+                    .addParameter("phone_number", manager.getPhone_number())
+                    .addParameter("email", manager.getEmail())
+                    .executeUpdate();
+        }
     }
 
     @Override
     public void deleteById(int id) {
+        String query = "DELETE FROM property_managers WHERE id = :id";
+        try(Connection conn = DB.sql20.open()){
+            conn.createQuery(query)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
 
     }
 
